93260a7911508e70df42c375265fae6949f5ac8f
AllenOu <allenseac@gmail.com>
2017-02-09 22:20:02 +0800

    Support control the snake crawl direction

--- main.js
+++ main.js
@@ -92,8 +92,8 @@ Snake.prototype = {
         for (let i = 0; i < snakeObj.length; i++) {
             this.snakeRectArray[i].draw()
         }
-        console.log('x' + this.head.x)
-        console.log('y' + this.head.y)
+        // console.log('x' + this.head.x)
+        // console.log('y' + this.head.y)
     },
     crawl() {
         let rect = new Rect(this.head.x, this.head.y, this.head.w, this.head.h, snakeObj.color)
@@ -115,12 +115,13 @@ Snake.prototype = {
             default:
                 break
         }
+
         if (this.head.x >= mapObj.width || this.head.y >= mapObj.height) {
             gameOver = confirm('game over')
             if (gameOver) {
                 snakeObj.crawling = false
                 cancelAnimationFrame(crawlID)
-                initGame()
+                food.draw()
             }
         }
     }
@@ -149,6 +150,7 @@ function handleCrawl() {
  *listen keyword
  */
 document.addEventListener('keydown', (e) => {
+    // control the snake crawl or stop
     if (e.keyCode === 32) {
         if (snakeObj.crawling) {
             console.log('stop')
@@ -160,4 +162,29 @@ document.addEventListener('keydown', (e) => {
             requestAnimationFrame(handleCrawl)
         }
     }
+    // control the snake crawl direction
+    switch (e.keyCode) {
+        //up:38 right:39 down:40 left:37
+        case 37:
+            if (snake.direction !== 39) {
+                snake.direction = 37
+            }
+            break
+        case 38:
+            if (snake.direction !== 40) {
+                snake.direction = 38
+            }
+            break
+        case 39:
+            if (snake.direction !== 37) {
+                snake.direction = 39
+            }
+            break
+        case 40:
+            if (snake.direction !== 38) {
+                snake.direction = 40
+            }
+            break
+    }
+    e.preventDefault()
 })
